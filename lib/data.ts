3617822770c76a61abff875c9443db0531090e28
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-04-02 14:59:00 -0400

    added jamify image aand title

--- lib/data.ts
+++ lib/data.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
+import jamifyScreenshot from "@/public/jamify-screenshot.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
@@ -70,11 +70,11 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Jamify",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    tags: ["React", "Redux", "PostgreSQL", "Tailwind", "Flask", "Python", "AWS", "S3"],
+    imageUrl: jamifyScreenshot,
   },
   {
     title: "rmtDev",
