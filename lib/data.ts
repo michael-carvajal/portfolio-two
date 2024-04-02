ab89a898502450a40b4c927e73a40aadb3ae3fbd
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-04-02 15:03:46 -0400

    added foxtrot and airbnb

--- lib/data.ts
+++ lib/data.ts
@@ -3,8 +3,8 @@ import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import jamifyScreenshot from "@/public/jamify-screenshot.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import foxTrot from "@/public/foxtrot-screenshot.png";
+import stayBnb from "@/public/staybnb-screenshot.png";
 
 export const links = [
   {
@@ -77,18 +77,18 @@ export const projectsData = [
     imageUrl: jamifyScreenshot,
   },
   {
-    title: "rmtDev",
+    title: "Foxtrot",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    tags: ["React", "Redux", "SQLAlchemy", "Github", "Agile", "Finance"],
+    imageUrl: foxTrot,
   },
   {
-    title: "Word Analytics",
+    title: "Stay BnB",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    imageUrl: stayBnb,
   },
 ] as const;
 
